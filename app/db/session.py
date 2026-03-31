a26ac46ef129471084651a464cc049e26927ed71
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2026-03-31 23:55:08 +0300

    refactor: move db url to constants

--- app/db/session.py
+++ app/db/session.py
@@ -1,12 +1,10 @@
-import os
-
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
     create_async_engine,
 )
 
-DATABASE_URL = os.environ["DATABASE_URL"]
+from app.constants import DATABASE_URL
 
 engine = create_async_engine(DATABASE_URL, echo=True)
 
