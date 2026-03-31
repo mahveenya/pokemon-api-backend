d0fe9478b56d4b6d56f16e72b82cc9302a777929
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2026-03-31 23:50:54 +0300

    chore: remove hardcoded db url

--- app/db/session.py
+++ app/db/session.py
@@ -1,10 +1,13 @@
+import os
+
 from sqlalchemy.ext.asyncio import (
     AsyncSession,
     async_sessionmaker,
     create_async_engine,
 )
 
-DATABASE_URL = "postgresql+asyncpg://admin:admin@db:5432/pokemon_db"
+DATABASE_URL = os.environ["DATABASE_URL"]
+
 engine = create_async_engine(DATABASE_URL, echo=True)
 
 AsyncSessionLocal = async_sessionmaker(
