e4f38ba2f93f30a47a82829e1aeacde11ec302cb
Kiryl Makhveyenia <mahveenya.k.a@gmail.com>
2026-04-06 19:35:47 +0300

    feat: add error responses

--- app/main.py
+++ app/main.py
@@ -10,7 +10,7 @@ from app.services.pokemon_service import get_pokemon_by_id, get_pokemons_list
 app = FastAPI()
 
 
-@app.get("/", response_model=PokemonListSchema)
+@app.get("/pokemon", response_model=PokemonListSchema)
 async def list_pokemons(
     offset: int = 0,
     limit: int = 20,
@@ -19,24 +19,43 @@ async def list_pokemons(
     return await get_pokemons_list(session, offset, limit)
 
 
-@app.get("/pokemon/{identifier}", response_model=PokemonSchema)
+@app.get(
+    "/pokemon/{id_or_name}",
+    response_model=PokemonSchema,
+    responses={
+        404: {"description": "Pokemon not found"},
+        501: {"description": "Search by name is not implemented yet"},
+    },
+)
 async def get_pokemon(
-    identifier: str, session=Depends(get_session)
+    id_or_name: str, session=Depends(get_session)
 ) -> PokemonSchema | None:
-    if identifier.isdigit():
-        pokemon = await get_pokemon_by_id(session, int(identifier))
+    if id_or_name.isdigit():
+        pokemon = await get_pokemon_by_id(session, int(id_or_name))
         if pokemon is None:
             raise HTTPException(status_code=404, detail="Pokemon not found")
         return pokemon
     else:
         raise HTTPException(
-            status_code=400, detail="Search by substring is not implemented yet"
+            status_code=501, detail="Search by name is not implemented yet"
         )
 
 
-@app.get("/ability/{ability_id}", response_model=AbilitySchema)
-async def get_ability(ability_id: int, session=Depends(get_session)) -> AbilitySchema:
-    ability = await get_ability_by_id(session, ability_id)
-    if ability is None:
-        raise HTTPException(status_code=404, detail="Ability not found")
-    return ability
+@app.get(
+    "/ability/{id_or_name}",
+    response_model=AbilitySchema,
+    responses={
+        404: {"description": "Ability not found"},
+        501: {"description": "Search by name is not implemented yet"},
+    },
+)
+async def get_ability(id_or_name: str, session=Depends(get_session)) -> AbilitySchema:
+    if id_or_name.isdigit():
+        ability = await get_ability_by_id(session, int(id_or_name))
+        if ability is None:
+            raise HTTPException(status_code=404, detail="Ability not found")
+        return ability
+    else:
+        raise HTTPException(
+            status_code=501, detail="Search by name is not implemented yet"
+        )
